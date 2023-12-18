d232bed70e91dd139bcbdebccf2d1ddbeaa6d7b5
danjwalton <20973540+danjwalton@users.noreply.github.com>
2023-12-18 13:03:13 +0000

    Final push

--- project_code/RRP requirements.py
+++ project_code/RRP requirements.py
@@ -1,3 +1,9 @@
+#Description: Pulls finance data on a per-country basis from UNHCR's terrible PowerBI portal. Requires Chromium Selenium.
+#Author: Dan W
+#Creation date: Feb 2021
+#Last revision:
+#Notes: The script iterates through all RRP appeals, by year, by country, on the PowerBI and records the requirements/funding on a per-country basis. The script can run into issues if your connection is slow, or if you alter the Chromium window which it opens. The script writes to a local csv output which is overwritten when the script is run.
+
 from time import sleep
 import json, os, datetime, re, sys, requests, random
 from urllib.request import urlopen
@@ -18,14 +24,14 @@ from selenium.webdriver.common.keys import Keys
 #Setup
 ###
 
-script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-chromedriver_dir = script_dir + '/project_data/chromedriver.exe'
-chrome_dir = script_dir + '/project_data/bin/chrome.exe'
+script_dir = os.path.dirname(os.path.abspath(__file__))
+chromedriver_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+chromedriver_dir = chromedriver_path + '/utilities/chromedriver/chromedriver.exe'
+chrome_dir = chromedriver_path + '/utilities/chromedriver/bin/chrome.exe'
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = chrome_dir
-chrome_options.add_argument("window-size=1920,1080")
+chrome_options.add_argument("window-size=800,600")
 chrome_options.add_argument('--disable-extensions')
 chrome_options.add_argument('--profile-directory=Default')
 chrome_options.add_argument("--incognito")
@@ -35,9 +41,8 @@ prefs = {"directory_upgrade": True, "extensions_to_open": "", "profile.default_c
 chrome_options.add_experimental_option("prefs", prefs)
 
 browser = webdriver.Chrome(chromedriver_dir, options=chrome_options)
-browser.implicitly_wait(30)
+browser.implicitly_wait(10)
 wait = WebDriverWait(browser, 10)
-action = webdriver.ActionChains(browser)
 
 ####
 #Functions
@@ -46,21 +51,22 @@ action = webdriver.ActionChains(browser)
 def get_home_rrps():
     global home_rrps_dropdown
     global home_rrps_search
-    home_rrps_search = browser.find_elements_by_xpath("//*[text()='Search']")[1].find_element_by_xpath('..')
-    home_rrps_dropdown = browser.find_element_by_xpath("//*[contains(@aria-label, 'Inter-Agency Regional Plan,')]")
+    home_rrps_search = browser.find_elements_by_xpath("//*[text()='Search']")[0].find_element_by_xpath('..')
+    home_rrps_dropdown = browser.find_element_by_xpath("//*[contains(@aria-label, 'Regional Plan')]/i")
     home_rrps_dropdown.click()
     home_rrps = browser.find_elements_by_xpath("//span[@class='slicerText' and contains(@title, 'RP')]")
     home_rrps_dropdown.click()
+    home_rrps_search = browser.find_elements_by_xpath("//*[text()='Search']")[0].find_element_by_xpath('..')
     return home_rrps
 
 def next_country(previous_country):
     global more_countries
     global rrp_countries
     global rrp_countries_dropdown
-    rrp_countries_dropdown = browser.find_elements_by_xpath("//*[@class='slicer-dropdown-menu']")[0]
+    rrp_countries_dropdown = browser.find_elements_by_xpath("//*[@class='slicer-dropdown-menu']")[1]
     rrp_countries_dropdown.click()
     sleep(0.2)
-    rrp_countries = browser.find_elements_by_xpath("//*[@class='slicer-dropdown-popup visual']//*[@class='slicerText']")
+    rrp_countries = browser.find_elements_by_xpath("//*[@class='slicer-dropdown-popup visual focused']//*[@class='slicerText']")
     
     if previous_country:
         rrp_countries = rrp_countries[rrp_countries.index(previous_country)+1:]
@@ -80,14 +86,14 @@ def next_country(previous_country):
 
 def get_rrp_years():
     browser.switch_to.default_content()
-    browser.switch_to.frame(2)
+    browser.switch_to.frame(0)
     rrp_years = browser.find_elements_by_xpath("//*[@class='slicerItemContainer']")
     browser.switch_to.default_content()
     return(rrp_years)
 
 def switch_year(year):
     global rrp_year
-    browser.switch_to.frame(2)
+    browser.switch_to.frame(0)
     rrp_year_btn = rrp_years[year]
     rrp_year = rrp_year_btn.text
     browser.execute_script("arguments[0].click();", rrp_year_btn) #because chromedriver can't click straight sometimes
@@ -110,9 +116,9 @@ url = "https://app.powerbi.com/view?r=eyJrIjoiM2M2NDMyZTgtZmQ4OC00ZDE1LWEyYWMtMT
 
 browser.get(url)
 
-output = pd.read_csv(script_dir + '/output/rrp_data.csv')
+output = pd.DataFrame()
+output.to_csv(script_dir + '/rrp_data.csv', index=False)
 
- 
 home_rrps = get_home_rrps()
 
 for i in range(len(home_rrps)):
@@ -123,7 +129,7 @@ for i in range(len(home_rrps)):
     sleep(0.2)
     rrp.click()
     home_rrps_dropdown.click()
-    home_rrps_search.click()
+    webdriver.ActionChains(browser).click(home_rrps_search).perform()
 
     sleep(3)
     rrp_years = get_rrp_years()
@@ -150,6 +156,7 @@ for i in range(len(home_rrps)):
             cells.columns = cols
 
             output = output.append(cells, ignore_index = True)
+            output.to_csv(script_dir + '/rrp_data.csv', index=False)
 
             previous_country = rrp_country
 
@@ -160,5 +167,5 @@ for i in range(len(home_rrps)):
     sleep(3)
     home_rrps = get_home_rrps()
 
-output.to_csv(script_dir + '/output/rrp_data.csv', index=False)
+output.to_csv(script_dir + '/rrp_data.csv', index=False)
 browser.quit()
